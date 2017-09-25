24a952e050c9d3b8e42a585823a1f3a1c2b0ba12
Garrett Kucinski <garrettryan@doejo.com>
2017-09-25 16:26:23 -0500

    finalized authentication

--- client/src/app/recommendations/recommendations.component.ts
+++ client/src/app/recommendations/recommendations.component.ts
@@ -14,9 +14,9 @@ export class RecommendationsComponent implements OnInit {
     ngOnInit() {
         // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         // Add 'implements OnInit' to the class.
-        this.dataService.getUser().subscribe(
-            data => this.user = data.user
-        );
+        // this.dataService.getUser().subscribe(
+        //     data => this.user = data.user
+        // );
 
         this.dataService.getTweets().subscribe(
             // tweets => this.tweets = tweets.result.data.statuses
