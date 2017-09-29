89cb05c7413459a372103c7aac9d642be9ea9264
Garrett Kucinski <garrettryan@doejo.com>
2017-09-29 00:30:00 -0500

    fixing error in path name

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/auth', authRoutes);
 
 // Catch all other routes and return the index file
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client2/dist')));
+  app.use(express.static(path.join(__dirname, 'client/dist')));
   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
